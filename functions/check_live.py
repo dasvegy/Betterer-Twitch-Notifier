4da0bee41e1efa134e5740ce691903e5f20a4189
dasvegy <konstantin.killer008@gmail.com>
2025-05-12 22:01:12 +0200

    - Now instead of Crashing you get an Error when the streamer was not found

--- functions/check_live.py
+++ functions/check_live.py
@@ -2,15 +2,24 @@ from functions.notify import send_notification
 from functions.colors import Colors
 import requests
 
+class StreamerNotFoundError(Exception):
+    def __init__(self, username):
+        super().__init__(f"Streamer '{username}' not found or returned no data.")
+
+
 def check_streamer_live(username):
     url = f"https://api.ivr.fi/v2/twitch/user?login={username}"
     headers = {"User-Agent": "Mozilla/5.0"}
 
     response = requests.get(url, headers=headers)
+
     if response.status_code == 200:
         print(f"Loading Streamer: {Colors.purple}{Colors.bold}{username}{Colors.reset}")
         print("Successful: code 200")
         data = response.json()
+
+        if not data:
+            raise StreamerNotFoundError(username)
 
         islive = data[0]["stream"]
 
@@ -23,4 +32,8 @@ def check_streamer_live(username):
         print(f"DONE LOADING STREAMER {Colors.purple}{Colors.bold}{username}{Colors.reset}")
         print("---------------------------------------------------------\n")
     else:
-        print("Error")
+        print(f"Loading Streamer: {Colors.purple}{Colors.bold}{username}{Colors.reset} \n")
+        print(f"{Colors.red}Error: API returned status code {response.status_code}{Colors.reset}")
+
+        print(f"\nDONE LOADING STREAMER {Colors.purple}{Colors.bold}{username}{Colors.reset}")
+        print("---------------------------------------------------------\n")
